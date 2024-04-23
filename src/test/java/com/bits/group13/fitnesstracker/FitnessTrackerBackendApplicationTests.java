e417102f5f5610946ebcdf8bd225623e6f34c0ce
Mohamed Safeuq <safeuq@xflowpay.com>
2024-04-23 23:32:40 +0530

    First commit

---
+++ src/test/java/com/bits/group13/fitnesstracker/FitnessTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.group13.fitnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
