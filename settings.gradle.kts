5aadd3892746a273129dab023880fdcc93dfb7d5
Mohamed Safeuq <2023mt93102@wilp.bits-pilani.ac.in>
2024-11-23 20:20:06 +0530

    Move users to this repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 rootProject.name = "fit-mind-be-core"
 
 include("fit-mind-commons")
-
 include("fit-mind-commons-api")
 include("fit-mind-core-be")
+include("fit-mind-user-api")
+include("fit-mind-user-be")
